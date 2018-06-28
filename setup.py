9c4170314587756918f9bc0e200e2acb5718982d
H. Kamran <hkamran@unisontech.org>
2018-06-28 02:11:39 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nettools',
+      version='1.0',
+      description='The networking toolkit for Python',
+      url='http://github.com/hkamran80/nettools',
+      author='H. Kamran',
+      author_email='hkamran@unisontech.org',
+      license='GPLv3+',
+      packages=['nettools'],,
+      install_requires=[
+          'ipwhois', 'dnspython3',
+      ],
+      zip_safe=False)
